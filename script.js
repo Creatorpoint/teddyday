1806c39ee7038754689418249139492bb1453a1f
Creatorpoint <pk4030794@gmail.com>
2026-02-10 04:36:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function yesClick(){
-  document.getElementById("reply").innerHTML =
-  "Thank you for choosing my heart 💖<br> I promise to keep you smiling always 🌸";
+function move(btn){
+  btn.style.position = "absolute";
+  btn.style.left = Math.random()*80 + "%";
+  btn.style.top = Math.random()*80 + "%";
+}
+
+function yes(){
+  document.getElementById("msg").innerHTML =
+    "YAYYY 🥳💖 Mujhe pata tha tum YES hi bologi 😘";
 }
